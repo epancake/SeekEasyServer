c934130caacbe10be8365c9a6366a32b850c955b
Emily Pancake <epancake24@gmail.com>
2017-12-04 16:17:47 -0700

    error add cors

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const data = require('./data/data');
-// const cors = require('cors')
+const cors = require('cors')
 
-// app.use(cors())
-//
+app.use(cors())
+
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*')
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
