110853ebeef23b6f8c4cd9a48b5bc1e08cd2f464
Emily Pancake <epancake24@gmail.com>
2017-12-04 11:42:53 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const data = require('./data/data');
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//   next()
+// })
+
+app.get('/', (req, res) => {
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log('listening here ', port);
+});
---
+++ data/data.js
@@ -0,0 +1,26 @@
+module.exports =
+  [{
+    'name': 'Le&ntilde;a',
+    'id': 1,
+    'Monday': '5 - 6pm',
+    'Tuesday': '5 - 6pm',
+    'Wednesday': '5 - 6pm',
+    'Thursday': '5 - 6pm',
+    'Friday': '4 - 6pm',
+    'Saturday': '4 - 6pm',
+    'Sunday': '5 - 6pm',
+    'type': 'Food and Drink'
+  },
+  {
+    'name': 'Gozo',
+    'id': 2,
+    'Monday': false,
+    'Tuesday': '3 - 6pm',
+    'Wednesday': '3 - 6pm',
+    'Thursday': '3 - 6pm',
+    'Friday': '3 - 6pm',
+    'Saturday': false,
+    'Sunday': false,
+    'type': 'Food and Drink'
+  },
+];
