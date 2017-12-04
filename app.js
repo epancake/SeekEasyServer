29c4148920cdf3b4616780c39d30e6e99fa86cf8
Emily Pancake <epancake24@gmail.com>
2017-12-04 16:12:27 -0700

    add data

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const data = require('./data/data');
+// const cors = require('cors')
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+// app.use(cors())
+//
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//   next()
+// })
 
 app.get('/', (req, res) => {
   res.json(data);
