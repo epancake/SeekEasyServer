28ae7b471a676c75714191281644999cac5120e1
Emily Pancake <epancake24@gmail.com>
2017-12-04 13:26:49 -0700

    add header

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 const data = require('./data/data');
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-//   next()
-// })
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 app.get('/', (req, res) => {
   res.json(data);
